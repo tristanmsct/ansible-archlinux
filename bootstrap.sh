da32a4c27a83e86b290ed3f01c815427c0fc479b
Tristan Muscat <tristan.muscat@pm.me>
2026-09-17 23:16:22 +0200

    Fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo pacman -S --noconfirm --needed python git ansible
+sudo pacman -S --noconfirm --needed git ansible
 git clone https://github.com/tristanmsct/ansible-archlinux.git "$HOME/.local/share/ansible-archlinux"
 cd "$HOME/.local/share/ansible-archlinux"
 ansible-playbook site.yml -K
